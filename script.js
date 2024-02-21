634ec9a3948d6d5624eee53010be9226d8d7005d
Julian Feliciano <jfelic@Julians-MacBook-Pro.local>
2024-02-21 14:46:03 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// Get random starting number
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber); //testing
+
+// Storing reference to HTML elements into variables. //
+// The contents in some of these HTML elements might change, 
+// but the reference that we are storing never does. //
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+
+function checkGuess() {
+      const userGuess = Number(guessField.value);
+      if (guessCount === 1) {
+        guesses.textContent = "Previous guesses:";
+      }
+      guesses.textContent = `${guesses.textContent} ${userGuess}`;
+    
+      if (userGuess === randomNumber) {
+        lastResult.textContent = "Congratulations! You got it right!";
+        lastResult.style.backgroundColor = "green";
+        lowOrHi.textContent = "";
+        setGameOver();
+      } else if (guessCount === 10) {
+        lastResult.textContent = "!!!GAME OVER!!!";
+        lowOrHi.textContent = "";
+        setGameOver();
+      } else {
+        lastResult.textContent = "Wrong!";
+        lastResult.style.backgroundColor = "red";
+        if (userGuess < randomNumber) {
+          lowOrHi.textContent = "Last guess was too low!";
+        } else if (userGuess > randomNumber) {
+          lowOrHi.textContent = "Last guess was too high!";
+        }
+      }
+    
+      guessCount++;
+      guessField.value = "";
+      guessField.focus();
+    }
+    
+
+// Add Even Listener to guessSubmit element
+guessSubmit.addEventListener("click", checkGuess);
+
+function setGameOver() {
+      guessField.disabled = true;
+      guessSubmit.disabled = true;
+      resetButton = document.createElement("button");
+      testing = document.createElement("h1");
+      testing.textContent = "Wassup";
+      resetButton.textContent = "Start new game";
+      document.body.append(resetButton);
+      resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame() {
+      guessCount = 1;
+
+}
+    
